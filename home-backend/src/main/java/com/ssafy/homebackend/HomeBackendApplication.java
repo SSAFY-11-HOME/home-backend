fb9f750d80d626218ff126877e749587edec5bf7
이강산 <soh7174@gmail.com>
2024-05-13 17:46:26 +0900

    init project

---
+++ home-backend/src/main/java/com/ssafy/homebackend/HomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.homebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBackendApplication.class, args);
+	}
+
+}
---
+++ home-backend/src/test/java/com/ssafy/homebackend/HomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.homebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
