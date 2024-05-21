2e2da916ed75c3cac0178db22b382f2b8bab779c
san <soh7174@gmail.com>
2024-05-22 02:21:52 +0900

    feat: 글 수정 + 로그인 #8

--- home-backend/src/main/java/com/ssafy/homebackend/controller/BoardQnAController.java
+++ home-backend/src/main/java/com/ssafy/homebackend/controller/BoardQnAController.java
@@ -40,7 +40,7 @@ public class BoardQnAController {
 	@Autowired
 	JWTUtil jwtUtil;
 	
-	@Operation(summary = "QnA 게시판 글 쓰기", description = "로그인 상태에서만 글 쓰기 가능. title, contents 받음.")
+	@Operation(summary = "QnA 게시판 글 쓰기", description = "로그인 상태에서만 글 쓰기 가능. header(Authorization:access_token 값), body(title, contents) 받음.")
 	@ApiResponses(value = { 
 			@ApiResponse(responseCode = "201", description = "QnA 글쓰기 성공"), 
 			@ApiResponse(responseCode = "400", description = "제목 또는 내용의 길이가 0임."),
@@ -111,10 +111,10 @@ public class BoardQnAController {
 		return new ResponseEntity<HashMap<String, Board>>(map, HttpStatus.OK);
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
-	@Operation(summary = "QnA 게시판 글 삭제", description = "자기 글만 삭제 가능. Header(accessToken), Body(id, articleId) 받음")
+	@Operation(summary = "QnA 게시판 글 삭제", description = "자기 글만 삭제 가능. header(Authorization:access_token 값), body(id, articleId) 받음")
 	@ApiResponses(value = { 
 			@ApiResponse(responseCode = "200", description = "QnA 글 삭제 성공"),
-			@ApiResponse(responseCode = "400", description = "accessToken에서 추출한 id와 글의 id가 다름. 자신의 글만 삭제 가능."),
+			@ApiResponse(responseCode = "403", description = "accessToken에서 추출한 id와 글의 id가 다름. 자신의 글만 삭제 가능."),
 			@ApiResponse(responseCode = "401", description = "accessToken 만료됨. /user/refresh로 refreshToken 전달해서 accessToken 갱신 후 재요청."),
 			@ApiResponse(responseCode = "404", description = "전달 받은 accessToken 없음. 로그인 필요. 로그인 페이지로 이동시키기.")
 			})
@@ -150,33 +150,59 @@ public class BoardQnAController {
 			
 		} else {
 			resultMap.put("message", "삭제 권한이 없습니다. 현재 로그인 한 사용자가 작성한 글이 아닙니다.");
-			status = HttpStatus.BAD_REQUEST;
+			status = HttpStatus.FORBIDDEN;
 			return new ResponseEntity<Map<String, Object>>(resultMap, status);
 		}
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	@Operation(summary = "QnA 게시판 글 수정", description = "자기 글만 수정 가능. articleId, id, title, contents 받음")
+	@Operation(summary = "QnA 게시판 글 수정", description = "자기 글만 수정 가능(admin도 글 수정 불가). header(Authorization:access_token 값), Body(id, articleId, title, contents) 받음")
 	@ApiResponses(value = { 
 			@ApiResponse(responseCode = "200", description = "QnA 글 수정 성공"),
-			@ApiResponse(responseCode = "400", description = "QnA 글 수정 실패")
+			@ApiResponse(responseCode = "400", description = "수정한 제목 또는 수정한 내용의 길이가 0임."),
+			@ApiResponse(responseCode = "401", description = "accessToken 만료됨. /user/refresh로 refreshToken 전달해서 accessToken 갱신 후 재요청."),
+			@ApiResponse(responseCode = "403", description = "accessToken에서 추출한 id와 글의 id가 다름. 자신의 글만 수정 가능."),
+			@ApiResponse(responseCode = "404", description = "전달 받은 accessToken 없음. 로그인 필요. 로그인 페이지로 이동시키기.")
 			})
 	@PutMapping
-	public ResponseEntity<String> update(@RequestBody Board board) {
-		if (board.getTitle().equals("") || board.getContents().equals("")) {
-			return new ResponseEntity<String>("모든 필드를 입력해 주세요.", HttpStatus.BAD_REQUEST);
+	public ResponseEntity<Map<String, Object>> update(@RequestBody Board board, HttpServletRequest request) {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		
+		// 액세스 토큰 자체가 없는 경우 == 로그인x 상태
+		String accessToken = request.getHeader("Authorization");
+		if(accessToken == null) {
+			System.out.println("토큰 없음. 로그인x 상태.");
+			resultMap.put("message", "전달받은 accessToken 없음! 로그인 하세요.");
+			status = HttpStatus.NOT_FOUND;
+			return new ResponseEntity<Map<String, Object>>(resultMap, status);
 		}
 		
-		String id = board.getId();
-		int articleId = board.getArticleId();
-		
-		
-		Board temp = boardQnAService.selectOne(articleId);
-		if(temp.getId().equals(id)==false) {
-			return new ResponseEntity<String>("자신의 글만 수정할 수 있습니다.", HttpStatus.BAD_REQUEST);
+		// 액세스 토큰은 넘어 온 상태. 해당 토큰 유효한 지 검사.
+		if (!jwtUtil.checkToken(accessToken)) {
+			System.out.println("accessToken 만료됨.");
+			resultMap.put("message", "accessToken 만료됨. /user/refresh 로 토큰 갱신 후 재요청.");
+			status = HttpStatus.UNAUTHORIZED;
+			return new ResponseEntity<Map<String, Object>>(resultMap, status);
 		}
 		
-		int result = boardQnAService.update(board);
-		return new ResponseEntity<String>("글 수정 성공", HttpStatus.OK);
+		// 유효한 액세스 토큰. 토큰에서 id 추출해서 현재 유저가 작성한 글인지 판단
+		String idInToken = jwtUtil.getUserId(accessToken);	
+		if (board.getId().equals(idInToken)) { // 현재 유저가 작성한 글일 때,
+			if (board.getTitle().trim().length()>0 && board.getContents().trim().length()>0) { 
+				int result = boardQnAService.update(board);
+				resultMap.put("message", "QnA 수정 성공.");
+				status = HttpStatus.OK;
+				return new ResponseEntity<Map<String, Object>>(resultMap, status);		
+			} else { // 제목 또는 내용의 길이가 0인 경우
+				resultMap.put("message", "제목 또는 내용의 길이가 0임.");
+				status = HttpStatus.BAD_REQUEST;
+				return new ResponseEntity<Map<String, Object>>(resultMap, status);
+			}
+		} else {
+			resultMap.put("message", "수정 권한이 없습니다. 현재 로그인 한 사용자가 작성한 글이 아닙니다.");
+			status = HttpStatus.FORBIDDEN;
+			return new ResponseEntity<Map<String, Object>>(resultMap, status);
+		}
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	@Operation(summary = "QnA 게시판 글 전체 불러오기", description = "권한 상관 없이 글 읽기 가능.")
